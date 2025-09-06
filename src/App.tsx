669111e159c1f28ff123507a4ec72b29abd12066
Hupno Technologies <160036189+hupnotech-gh@users.noreply.github.com>
2025-09-06 21:15:13 +0000

    feat: migrate to Vite React TS TailwindCSS website with localStorage support

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout/Layout";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Industries from "./pages/Industries";
+import CaseStudies from "./pages/CaseStudies";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/industries" element={<Industries />} />
+          <Route path="/case-studies" element={<CaseStudies />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
+}
+
+export default App;
